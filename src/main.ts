93cddff0e84f74009e4045b4c5d30b9685aaf40a
Ryan Christian SR <ryancristiansilva551@gmail.com>
2024-10-28 08:26:21 -0400

    atz

--- src/main.ts
+++ src/main.ts
@@ -21,22 +21,43 @@ app.get("/produtos",async(req,res)=>{
     try{
         const conexao = await mysql.createConnection({
             host: process.env.dbhost?process.env.dbhost:"localhost",
-            user: process.env.dbuser?process.env.dbuser:"root",
-            password: process.env.dbpassword?process.env.dbpassword:"",
-            database: process.env.dbname?process.env.dbname:"banco1022b",
-            port: process.env.dbport?parseInt(process.env.dbport):3306
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022b",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
         })
         //PASSO 3: QUERY  -> SELECT * FROM produtos
-        const [result,fields] = await conexao.query("SELECT * from produtos")
+        const [result,fields]  = await conexao.query("SELECT * FROM produtos")
         await conexao.end()
         //PASSO 4: Colocar os dados do banco no response
         res.send(result)
     }catch(e){
         res.status(500).send("Erro do servidor")
-    }
-    
-    
+    }  
 })
+
+app.post("/produtos",async(req,res)=>{
+    try{
+        const conexao = await mysql.createConnection({
+            host: process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022b",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        const {id,nome,descricao,preco,imagem} = req.body
+        const [result,fields]  = 
+            await conexao.query("INSERT INTO produtos VALUES (?,?,?,?,?)",
+                [id,nome,descricao,preco,imagem])
+        await conexao.end()
+        res.send(result)
+    }catch(e){
+        console.log(e)
+        res.status(500).send("Erro do servidor")
+    }  
+})
+
+
 
 
 //INICIAR O SERVIDOR
