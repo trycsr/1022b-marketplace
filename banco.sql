216e0d061787fd467fe3ad610485a2fe670efe8e
Ryan Christian SR <ryancristiansilva551@gmail.com>
2024-11-01 08:03:30 -0400

    mudei a estrutura

--- banco.sql
+++ banco.sql
@@ -1,17 +1,26 @@
-drop database if exists defaultdb;
+drop database IF EXISTS defaultdb;
 create database defaultdb;
-use defaultdb; 
-create table produtos(
-id BIGINT PRIMARY KEY AUTO_INCREMENT,
-nome varchar(300),
-descricao varchar(300),
-preco decimal(10,2),
-imagem varchar(300)
+USE defaultdb;
+CREATE TABLE produtos(
+	id BIGINT PRIMARY KEY,
+    nome VARCHAR(50),
+    descricao VARCHAR(50),
+    preco DECIMAL(10,2),
+    imagem VARCHAR(300)
 );
-create table usuarios(
-id int primary key,
-nome varchar(300),
-email varchar(300),
-created_at timestamp,
-updated_at timestamp
+
+CREATE TABLE usuarios(
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(50),
+    create_at TIMESTAMP,
+    updated_at TIMESTAMP
 );
+
+
+INSERT INTO produtos VALUES
+(1,'Iphone', 'Celular RUIM', 5000.50, 'sem imagem');
+
+INSERT INTO usuarios VALUES ('1', 'Gabriel Silva', 'gabriel2.silva@gmail.com', '2024-11-22 12:06:00', '2024-12-22 13:30:00');
+INSERT INTO usuarios VALUES ('2', 'Rodrigo Santos', 'rodrigo82santos@gmail.com', '2024-11-23 12:10:00', '2024-12-23 14:15:00');
+INSERT INTO usuarios VALUES ('3', 'Vanessa Martins', 'vanessa.martins18@gmail.com', '2024-11-24 12:30:00', '2024-12-24 14:30:00');
